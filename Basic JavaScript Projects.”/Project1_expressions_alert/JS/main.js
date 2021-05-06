e698b5ba2585721b357d8323cb633423be66b248
Dominique Taylor <123dominique.taylor@gmail.com>
2021-05-06 16:02:47 -0400

    first javascript submission assignment

---
+++ Basic JavaScript Projects.”/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert("JavaScript is Awesome");
+
+var Family="The Taylors", Dad="Jamal", Mom="Shatona", Son="Dominique", Daughter="Nala";
+
+
+var blues=" I have the blues.";
+var blues=blues.fontcolor("blue");
+
+var Mom=Mom.fontcolor("purple");
+var Dad=Dad.fontcolor("red");
+var Son=Son.fontcolor("darkblue");
+document.write(Son,Mom,Dad)
+document.write("<br>")
+document.write(3+3);
+document.write("<br>")
+document.write("\"People say when life gives you lemons, you make lemonade."+" I say when life gives you lemons, you make lemon smoothies.\""+"-Dominique Taylor");
