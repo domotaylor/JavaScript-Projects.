1b1cff2e8dd0f71881f8e886fdd0b45b7609423f
Dominique Taylor <123dominique.taylor@gmail.com>
2021-05-06 23:36:35 -0400

    functions submission

---
+++ Basic JavaScript Projects.”/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("JavaScript is Awesome");
+
+function My_First_Function() {
+    var str ="A&T colors are blud and Gold !";
+    var results=str.fontcolor("gold");
+    document.getElementById("Gold_Color").innerHTML=results;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
