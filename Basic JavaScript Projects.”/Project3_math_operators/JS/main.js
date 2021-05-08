24953fd11d2856bb54adaad8a9ac58279de888f4
Dominique Taylor <123dominique.taylor@gmail.com>
2021-05-08 02:52:52 -0400

    math operation

--- Basic JavaScript Projects.”/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects.”/Project3_math_operators/JS/main.js
@@ -2,3 +2,27 @@ function addition_Function() {
     var addition = 3+7;
     document.getElementById("math").innerHTML="3+7="+addition;
 }
+
+function Subtraction_Function() {
+    var Subtraction =10-5;
+    document.getElementById("math").innerHTML="10-5="+Subtraction;
+}
+
+function multiplication() {
+    var simple_math=5*5;
+    document.getElementById("math").innerHTML="5*5="+simple_math;
+}
+
+function division() {
+    var simple_math=25/5;
+    document.getElementById("math").innerHTML="25/5="+simple_math;
+}
+window.alert(Math.random());
+
+var B =5;
+B++;
+document.write(B)
+
+var A=12;
+A--;
+document.write(A);
