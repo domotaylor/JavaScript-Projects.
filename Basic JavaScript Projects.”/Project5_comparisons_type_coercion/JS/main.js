4e7c2acac4993331659b161634ffca53e6fdf9e1
Dominique Taylor <123dominique.taylor@gmail.com>
2021-05-10 01:23:04 -0400

    project5

---
+++ Basic JavaScript Projects.”/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,44 @@
+document.write("15" + 5);
+document.write("<br>");
+document.write(typeof 3);
+function My_Function() {
+    document.getElementById("Test").innerHTML=0/0;
+}
+
+function My_Function() {
+    document.getElementById("Test").innerHTML=isNaN('This is a string');
+}
+
+function My_Function() {
+    document.getElementById("Test").innerHTML=isNaN('007');   
+}
+
+function not_function() {
+    document.getElementById("Not").innerHTML=!(20>10);
+}
+document.write("<br>");
+document.write(2E310);
+document.write("<br>");
+document.write(-3E310);
+document.write("<br>");
+document.write(10<2);
+document.write("<br>");
+document.write(10>2);
+document.write("<br>");
+console.log(12<9);
+document.write("<br>");
+Document.write(10==10);
+document.write("<br>");
+document.write(5==12);
+document.write("<br>");
+X = 15;
+Y = 15;
+document.write(X===Y);
+document.write("<br>");
+document.write(5>2 && 10>4)
+document.write("<br>");
+document.write(5>10 || 10>4);
+document.write("<br>");
+var D = "The number six looks like" + "6"
+document.write(D);
+
