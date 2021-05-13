06e2050a49883b850edc25edc2b31919e19917ba
Dominique Taylor <123dominique.taylor@gmail.com>
2021-05-12 23:04:27 -0400

    project10 loops and arrays

--- Basic JavaScript Projects.”/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects.”/Project10_loops_arrays/JS/main.js
@@ -8,7 +8,7 @@ function Call_Loop() {
     }
     document.getElementById("Loop").innerHTML = Digit;
 }
-//A “For Loop” is used to repeat a section of code a number of times. For Loops are used when the number of iterations are known.
+//“For Loop” is used to repeat a section of code a number of times. For Loops are used when the number of iterations are known.
 var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
 var Content ="";
 var Y;
@@ -18,3 +18,48 @@ function for_Loop() {
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
+//Arrays are objects and, so, are included in the object data type. Objects can have properties (characteristics) and methods (actions).
+function array_Function() {
+    var Dog_Picture = [];
+    Dog_Picture[0] = "sleeping";
+    Dog_Picture[1] = "playing";
+    Dog_Picture[2] = "eating";
+    Dog_Picture[3] = "barking";
+    document.getElementById("Array").innerHTML = "in this picture, the dog is " +
+        Dog_Picture[2] + ".";
+}
+//The name const is slightly misleading because it doesn’t technically create a “constant” (something immutable). Basically, the value assigned by the const keyword is not immutable (it can change) – yet, the variable identifier cannot be reassigned
+function constant_function() {
+    const Musical_Instrument = {type:"guitar",brand:"Fender",color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " +
+        Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
+//The let keyword declares variables that can have block scope. As a reminder, this refers to blocks of code contained within curly brackets – commonly loops
+var X = 82;
+document.write(X);
+{
+    let X = 33
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+var X =82;
+document.write(X);
+{
+    var  X = 33
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+//Methods are actions that can be performed on objects. In JavaScript, they are stored within properties as functions. One way to do this is utilizing the return statement and “this” keyword.
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "blue ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML =car.description();
